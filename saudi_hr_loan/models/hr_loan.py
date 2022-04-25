6c21055dd5d50889bcbcaaf316ede19de7508bbe
Eman <emanahmed4689@gmail.com>
2022-04-26 00:35:23 +0200

    [Add] add

--- saudi_hr_loan/models/hr_loan.py
+++ saudi_hr_loan/models/hr_loan.py
@@ -9,6 +9,14 @@ from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 from dateutil.relativedelta import relativedelta
 
+class HrEmployeePublic(models.Model):
+    _inherit = 'hr.employee.public'
+    address_home_id = fields.Many2one(
+        'res.partner', 'Address', help='Enter here the private address of the employee, not the one linked to your company.',
+        groups="hr.group_hr_user", tracking=True,
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
+
+
 
 
 class HrLoan(models.Model):
